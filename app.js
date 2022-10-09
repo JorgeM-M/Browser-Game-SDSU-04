56d1a80c89a37a606f6b79c51403df0e204e13ed
JorgeM-M <108438082+JorgeM-M@users.noreply.github.com>
2022-10-08 18:54:30 -0700

    created player

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
+
+canvas.width = 64 * 16 //1024
+canvas.height = 64 * 9 //576
+
+const player = new Player()
+
+//let bottom = y + 100
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "grey"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+    player.update()
+}
+
+animate()
